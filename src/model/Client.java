4acb4eb89df79bc50e950102e777688be9f3bd46
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-09-22 17:25:23 +0100

    add file DBconfig and add content model Person and client

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Client {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client  extends  Person{
+
+    private List<Contrat> contrats = new ArrayList<>();
+    private Conseiller conseiller;
+
+    public Client (String nom, String prenom,String email, Conseiller conseiller){
+        super(nom,prenom,email);
+        this.conseiller=conseiller;
+    }
+
+    public Conseiller getConseiller() {
+        return conseiller;
+    }
+
+    public void setConseiller(Conseiller conseiller) {
+        this.conseiller = conseiller;
+    }
 }
---
+++ src/resources/DBConfig.java
@@ -0,0 +1,22 @@
+package resources;
+
+public class DBConfig {
+    private static final String URL = "jdbc:mysql://localhost:3306/AssurTeche";
+    private static final String USERNAME = "root";
+    private static final String Password = "";
+    private static  final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static String getUsername(){
+        return USERNAME;
+    }
+    public static String getUrl(){
+        return USERNAME;
+    }
+    public static String getPassword(){
+        return USERNAME;
+    }
+    public static String getDriver(){
+        return USERNAME;
+    }
+
+}
