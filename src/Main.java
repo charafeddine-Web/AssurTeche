2a03efaf5d0c886cfb10b64cdb353b73d96745e2
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-09-23 11:13:06 +0100

    add connection avec JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import  resources.DBConfig;
+
+import java.sql.Connection;
+
+
 public class Main {
     public static void main(String[] args) {
+        DBConfig db=DBConfig.getInstance();
+        Connection conn = db.getConnection();
 
+        if(conn !=  null){
+            System.out.println("✅ Connexion réussie !");
+        } else {
+            System.out.println("❌ Connexion échouée !");
+        }
     }
 }
--- src/resources/DBConfig.java
+++ src/resources/DBConfig.java
@@ -1,23 +1,37 @@
 package resources;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/AssurTeche";
     private static final String USERNAME = "root";
-    private static final String Password = "root";
-    private static  final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    public static String getUsername(){
-        return USERNAME;
+    private static DBConfig instence;
+    private Connection connection;
+
+
+    private DBConfig(){
+        try{
+            Class.forName(DRIVER);
+            this.connection= DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
     }
-    public static String getUrl(){
-        return URL;
-    }
-    public static String getPassword(){
-        return Password;
-    }
-    public static String getDriver(){
-        return DRIVER;
+    public static  DBConfig getInstance(){
+        if (instence == null){
+            return new DBConfig();
+        }
+        return instence;
     }
 
+    public Connection getConnection() {
+        return connection;
+    }
 
 }
