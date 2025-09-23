495924c99add3b61c1e3edbaa7189fd811c53edb
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-09-23 17:28:54 +0100

    add function de addConseiller

--- src/DAO/ConseillerDAO.java
+++ src/DAO/ConseillerDAO.java
@@ -1,4 +1,38 @@
 package DAO;
 
+import model.Client;
+import model.Conseiller;
+import resources.DBConfig;
+
+import java.sql.*;
+
 public class ConseillerDAO {
+
+    private Connection conn;
+
+    public ConseillerDAO(){
+        this.conn= DBConfig.getInstance().getConnection();
+    }
+
+    public void addConseiller(Conseiller conseiller){
+
+        String sql="INSERT INTO Conseiller(nom,prenom,email) VALUES(?,?,?)";
+        try(PreparedStatement prs= conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
+            prs.setString(1, conseiller.getNom());
+            prs.setString(2,conseiller.getPrenom());
+            prs.setString(3,conseiller.getEmail());
+            prs.executeUpdate();
+            try(ResultSet res=prs.getGeneratedKeys()){
+                if(res.next()){
+                    conseiller.setId(res.getInt(1));
+                }
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
